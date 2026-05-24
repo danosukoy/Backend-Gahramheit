2ad045321bb7f34f1cb355eb5313b7a51e036b45
Miguel Villena Bustamante <miguel.villena.b@utec.edu.pe>
2026-05-23 21:40:47 -0500

    Primer Commit

---
+++ src/main/java/com/example/gahramheit/GahramheitApplication.java
@@ -0,0 +1,13 @@
+package com.example.gahramheit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GahramheitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GahramheitApplication.class, args);
+    }
+
+}
