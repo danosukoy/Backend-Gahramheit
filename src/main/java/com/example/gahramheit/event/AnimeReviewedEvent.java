889fdfbcb2a42e7cd2027f3f44ba8870e9295ae0
BrandoVM <xdlolbrando123@gmail.com>
2026-05-25 00:59:23 -0500

    Asicronia de AuthService

--- src/main/java/com/example/gahramheit/event/AnimeReviewedEvent.java
+++ src/main/java/com/example/gahramheit/event/AnimeReviewedEvent.java
@@ -5,12 +5,10 @@ import lombok.Getter;
 @Getter
 public class AnimeReviewedEvent {
     private final Long animeId;
-    private final String username;
     private final Integer score;
 
-    public AnimeReviewedEvent(Long animeId, String username, Integer score) {
+    public AnimeReviewedEvent(Long animeId, Integer score) {
         this.animeId = animeId;
-        this.username = username;
         this.score = score;
     }
 }
